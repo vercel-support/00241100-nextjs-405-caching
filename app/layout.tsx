bc084d60a8f5a5cea88576b41beb23c8f4dc86e6
Nick Vigier <nvigier46@gmail.com>
2024-06-11 10:07:02 +0100

    Add links to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        <Link href='/1'>1</Link>
+        <Link href='/2'>2</Link>
+        {children}
+      </body>
     </html>
   );
 }
