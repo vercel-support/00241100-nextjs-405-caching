a34ebbc9c8720cafd58eb76c75b51e2add856c01
Nick Vigier <nvigier46@gmail.com>
2024-06-13 08:30:46 +0100

    testing image hosted on blob

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'fazldsgjzbaj9202.public.blob.vercel-storage.com',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
